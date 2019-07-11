24fbdbca02a01682256cd7c10a1a9b8fb99f6db5
macshop <macone@MacBook-Pro-cua-mac.local>
2019-07-11 15:18:37 +0700

    only input text is number

--- index.js
+++ index.js
@@ -51,11 +51,15 @@ export default class TextInputMask extends Component {
     }
 
     if (nextProps.mask && (this.props.value !== nextProps.value)) {
-      // mask(this.props.mask, '' + nextProps.value, this.precision, text => {
-      let text = this._formatNumber(nextProps.value, this.precision);
+      let text = this._formatNumber(nextProps.value, this.precision) || '';
       this.input && this.input.setNativeProps({ text })
-      this.setState({ value: text });
-      // });
+      if(this.props.maxLength){
+        if(text.length <= this.props.maxLength){
+          this.setState({ value: text });
+        }
+      }else{
+        this.setState({ value: text });
+      }
     }
   }
 
@@ -127,9 +131,9 @@ export default class TextInputMask extends Component {
     multiline={this.props.mask && Platform.OS === 'ios' ? false : this.props.multiline}
     onChangeText={masked => {
       if (this.props.mask) {
-        let unmasked = masked.replace(/,|-/g, '');
+        let unmasked = masked.replace(/[^0-9.]/g,'');
         unmasked = this._formatNumber(unmasked, this.precision);
-        unmasked = unmasked.replace(/,|-/g, '');
+        unmasked = unmasked.replace(/[^0-9.]/g, '');
         this.props.onChangeText && this.props.onChangeText(masked.trim(), unmasked.trim())
       } else {
         this.props.onChangeText && this.props.onChangeText(masked.trim())
